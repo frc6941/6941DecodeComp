0050a23124d1969d01298eee51e35286feaa47a2
Mechaniac <AmberLiu2008@outlook.com>
2026-02-06 01:34:05 +0800

    Increase indexer pulse power for reliable shooter feed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/BeamBreakSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/BeamBreakSubsystem.java
@@ -1,63 +1,30 @@
-//package org.firstinspires.ftc.teamcode.Subsystems;
+package org.firstinspires.ftc.teamcode.Subsystems;
 
-//import com.arcrobotics.ftclib.command.SubsystemBase;
-//import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-//public class BeamBreakSubsystem extends SubsystemBase {
+public class BeamBreakSubsystem extends SubsystemBase {
 
-    //private final AnalogInput beamBreak;
-    //private final double defaultToleranceVoltage;
+    private final AnalogInput beamBreak;
+    private final double defaultToleranceVoltage;
 
-    //public BeamBreakSubsystem(HardwareMap hardwareMap, String name, double defaultToleranceVoltage) {
-        //beamBreak = hardwareMap.get(AnalogInput.class, name);
-        //this.defaultToleranceVoltage = defaultToleranceVoltage;
-    //}
+    public BeamBreakSubsystem(final HardwareMap hardwareMap,
+                              final String name,
+                              final double defaultToleranceVoltage) {
+        beamBreak = hardwareMap.get(AnalogInput.class, name);
+        this.defaultToleranceVoltage = defaultToleranceVoltage;
+    }
 
+    public boolean isBeamBreakOn() {
+        return isBeamBreakOn(defaultToleranceVoltage);
+    }
 
-    //public boolean isBeamBreakOn() {
-        //return isBeamBreakOn(defaultToleranceVoltage);
-    //}
+    public boolean isBeamBreakOn(final double toleranceVoltage) {
+        return beamBreak.getVoltage() >= toleranceVoltage;
+    }
 
-    //public boolean isBeamBreakOn(double toleranceVoltage) {
-       // return beamBreak.getVoltage() >= toleranceVoltage;
-    //}
-
-    //public double getVoltage() {
-        //return beamBreak.getVoltage();
-    //}
-//}
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.AnalogInput;
-
-@TeleOp(name = "Beam Break Debug", group = "Debug")
-public class debug_beam_break extends LinearOpMode {
-
-    private AnalogInput beamBreak;
-
-    @Override
-    public void runOpMode() {
-        // 初始化光电门
-        beamBreak = hardwareMap.get(AnalogInput.class, "shooterBB");
-
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-            // 读取电压值
-            double voltage = beamBreak.getVoltage();
-
-            telemetry.addData("Beam Break Voltage", "%.3f V", voltage);
-            telemetry.addData("Beam Break Status", voltage >= 1.0 ? "BLOCKED" : "CLEAR");
-            telemetry.addData("Expected Range", "0.0V - 3.3V");
-            telemetry.update();
-
-            sleep(100);
-        }
+    public double getVoltage() {
+        return beamBreak.getVoltage();
     }
 }
