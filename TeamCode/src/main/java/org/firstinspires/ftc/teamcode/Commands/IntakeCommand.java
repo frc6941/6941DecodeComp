4f8a4883283993ed1e089860f6c0fc303d088f0f
Mechaniac <AmberLiu2008@outlook.com>
2026-02-05 20:46:03 +0800

    Pulse indexer on beam-break trigger during intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/IntakeCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/IntakeCommand.java
@@ -12,6 +12,10 @@ public class IntakeCommand extends CommandBase {
     private final FeederSubsystem feeder;
     private BooleanSupplier breamBreak;
     private double intakePower, indexPower;
+    private static final long INDEX_FEED_PULSE_MS = 260L;
+
+    private boolean previousBeamBlocked;
+    private long indexFeedUntilNanos;
 
     public IntakeCommand(final FeederSubsystem feeder, BooleanSupplier breamBreak) {
         this(feeder, breamBreak, Constants.Feeder.DEFAULT_INTAKE_POWER, Constants.Feeder.DEFAULT_INDEX_POWER);
@@ -27,15 +31,28 @@ public class IntakeCommand extends CommandBase {
 
     @Override
     public void initialize() {
+        previousBeamBlocked = breamBreak.getAsBoolean();
+        indexFeedUntilNanos = 0L;
+
         feeder.setIntakeOpenLoop(intakePower);
-        feeder.setIndexOpenLoop(indexPower);
+        feeder.setIndexOpenLoop(0.0);
     }
 
     @Override
     public void execute() {
-        if (breamBreak.getAsBoolean()) {
-            feeder.setIndexOpenLoop(0.0);
+        final boolean beamBlocked = breamBreak.getAsBoolean();
+        final long nowNanos = System.nanoTime();
+
+        if (beamBlocked && !previousBeamBlocked) {
+            indexFeedUntilNanos = nowNanos + INDEX_FEED_PULSE_MS * 1_000_000L;
         }
+
+        final boolean shouldFeedIndexer = nowNanos < indexFeedUntilNanos;
+
+        feeder.setIntakeOpenLoop(intakePower);
+        feeder.setIndexOpenLoop(shouldFeedIndexer ? indexPower : 0.0);
+
+        previousBeamBlocked = beamBlocked;
     }
 
 
@@ -49,4 +66,3 @@ public class IntakeCommand extends CommandBase {
         return false;
     }
 }
-
